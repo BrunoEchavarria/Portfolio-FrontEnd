993b0c9055b6396d6141ba76d73e92705b8de49e
Bruno Echavarria <brunoechavarria18@gmail.com>
2023-01-23 19:25:46 -0300

    Agregandoarchivo main.js y refacts varias

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+const typed = new Typed('.typed' , {
+    // strings: [
+    //     '<i class="txt-change"> Developer. </i>' ,
+    //     '<i class="txt-change"> Designer. </i>' ,
+    //     '<i class="txt-change"> Coding. </i>'
+    //     ],
+
+        stringsElement: '#cadenas-texto',
+        typeSpeed: 90,
+        startDelay: 300,
+        backSpeed: 90,
+        smartBacksapace: true,
+        shuffle: false,
+        backDelay: 2000,
+        loop: true,
+        loopCount: false,
+        showCurso: true,
+        cursorChar: '_',
+        contentType: 'html'
+
+})
+
+
